4611afd8b5e35cce0b1e6e183372197de88b26fd
cosilayton <54328882+cosilayton@users.noreply.github.com>
2021-04-12 14:07:39 -0700

    firebase & firebase-auth

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuthProvider, FirebaseAuthConsumer } from '@react-firebase/auth';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,22 +12,42 @@ import {
 import Home from './Home';
 import Login from './Login';
 
+const config = {
+  appId: "1:1096449099:web:771193a7a07667083761ce",
+  apiKey: "",
+  projectId: "cool-react-game",
+  authDomain: "cool-react-game.firebaseapp.com",
+};
+
+console.log(config);
+
 const App = () => (
   <div className="app">
-    <Router>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
-      </nav>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <FirebaseAuthProvider firebase={firebase} {...config}>
+      <FirebaseAuthConsumer>
+      {({ isSignedIn, user, providerId }) => {
+    return (
+      <pre style={{ height: 300, overflow: "auto" }}>
+        {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+      </pre>
+    );
+  }}
+      </FirebaseAuthConsumer>
+      <Router>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </FirebaseAuthProvider>
   </div>
 );
 
