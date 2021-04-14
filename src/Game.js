0123ac714b7b8358014ab46e351e1a813b7b42ec
cosilayton <54328882+cosilayton@users.noreply.github.com>
2021-04-14 16:33:17 -0700

    Dynamic board + 1 initial test block

--- src/Game.js
+++ src/Game.js
@@ -5,22 +5,31 @@ const SHAPES = {
     BAR_2_1: 2
 };
 
+const BOARD_WIDTH = 70;
+const BOARD_HEIGHT = 30;
+
 const TICK_EVERY_MS = 1500;
 
-class Game extends React.Component {
+const EMPTY_BOARD = () => {
+    const board = [];
+    for (let i = 0; i < BOARD_HEIGHT; i++) {
+        const row = [];
+        for (let j = 0; j < BOARD_WIDTH; j++) {
+            row[j] = ' ';
+        }
+        board.push(row);
+    }
+    return board;
+}
 
+class Game extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             started: false,
             paused: false,
-            board: [
-                ['1', '1', ' '],
-                ['1', '1', ' '],
-                [' ', '1', ' '],
-                ['2', '3', '3']
-            ],
+            board: EMPTY_BOARD(),
             currentBlock: null
             // { color: 1, shape: SHAPES.SQUARE_2_2 }
         };
@@ -31,9 +40,20 @@ class Game extends React.Component {
         clearInterval(this.intervalId);
     }
 
+    nextBlock() {
+        const { board } = this.state;
+        // SQUARE_2_2
+        board[0][0] = '1';
+        board[0][1] = '1';
+        board[1][0] = '1';
+        board[1][1] = '1';
+        this.setState({ board });
+    }
+
     start = () => {
         this.setState({ started: true });
         this.intervalId = setInterval(this.tick, TICK_EVERY_MS);
+        this.nextBlock();
     }
 
     pause = () => {
@@ -52,12 +72,19 @@ class Game extends React.Component {
     }
 
     render() {
-        const { started, paused } = this.state;
+        const { board, started, paused } = this.state;
         return (
             <div>
                 <button onClick={this.start} disabled={started}>Start</button>
                 <button onClick={this.pause} disabled={!started || paused}>Pause</button>
                 <button onClick={this.resume} disabled={!started || !paused}>Resume</button>
+                <table className='board'>
+                  <tbody>
+                    {board.map((row, j) => (
+                        <tr key={j}>{row.map((color, i) => (<td key={i} className={`c${color}`}></td>))}</tr>
+                    ))}
+                  </tbody>
+                </table>
             </div>
         );
     }
