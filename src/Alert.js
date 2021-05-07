c7be46b458b1cbab2ba3a89f3b4171664664eede
cosilayton <54328882+cosilayton@users.noreply.github.com>
2021-05-06 17:06:27 -0700

    Play the game - make button and green

--- src/Alert.js
+++ src/Alert.js
@@ -5,14 +5,11 @@ const Alert = ({ message, onClose }) => (
       <div className="modal-background"></div>
       <div className="modal-card">
         <header className="modal-card-head">
-          <p className="modal-card-title">Application Alert</p>
+          <p className="modal-card-title">{message}</p>
           <button className="delete" aria-label="close" onClick={onClose}></button>
         </header>
-        <section className="modal-card-body">
-            { message }
-        </section>
         <footer className="modal-card-foot">
-          <button className="button is-success" onClick={onClose}>Close</button>
+            <button className="button is-success" onClick={onClose}>Close</button>
         </footer>
       </div>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Home = ({ user }) => (
   <section className='home'>
-      {user && <Link to='/game'>Play the Game</Link>}
+      {user && <Link className='button is-success' to='/game'>Play the Game</Link>}
       {(user === null) && <span>Please <Link to='/login'>login</Link> to play the Game</span>}
   </section>
 );
